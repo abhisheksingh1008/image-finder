b301b1c52814d6d7ba180bd4ece42edda1bdc231
unknown <ayushtwinkle937@gmail.com>
2024-01-22 14:13:45 +0530

    Feature: added download fucntionality

--- components/DownloadImage.js
+++ components/DownloadImage.js
@@ -1,13 +1,42 @@
 "use client";
 
-import { useState } from "react";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 
 const DownloadImage = ({ image }) => {
   const [width, setWidth] = useState(180);
+  const [imageBlob, setImageBlob] = useState(null);
+  const [downloadURL, setDownloadURL] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [largeImageSelected, setLargeImageSelected] = useState(false);
 
-  let downloadUrl = image.webformatURL.split("_")[0];
+  let imageUrl = image.webformatURL.split("_")[0];
+
+  useEffect(() => {
+    const generateDownloadURL = async () => {
+      const url = largeImageSelected
+        ? image.largeImageURL
+        : `${imageUrl}_${width}.jpg`;
+
+      // console.log(largeImageSelected, width, url);
+
+      setLoading(true);
+      await fetch(url)
+        .then((response) => response.arrayBuffer())
+        .then((buffer) => {
+          setImageBlob(new Blob([buffer], { type: "image/jpeg" }));
+        });
+      setLoading(false);
+    };
+
+    generateDownloadURL();
+  }, [width]);
+
+  useEffect(() => {
+    if (imageBlob) {
+      setDownloadURL(URL.createObjectURL(imageBlob));
+    }
+  }, [imageBlob]);
 
   return (
     <Box>
@@ -37,6 +66,7 @@ const DownloadImage = ({ image }) => {
                       checked={width === 180}
                       onClick={() => {
                         setWidth(180);
+                        setLargeImageSelected(false);
                       }}
                     />
                   </label>
@@ -55,6 +85,7 @@ const DownloadImage = ({ image }) => {
                       name="optradio"
                       onClick={() => {
                         setWidth(340);
+                        setLargeImageSelected(false);
                       }}
                     />
                   </label>
@@ -73,6 +104,7 @@ const DownloadImage = ({ image }) => {
                       name="optradio"
                       onClick={() => {
                         setWidth(640);
+                        setLargeImageSelected(false);
                       }}
                     />
                   </label>
@@ -81,7 +113,7 @@ const DownloadImage = ({ image }) => {
             </tr>
             <tr>
               <td>Original</td>
-              <td>960x720</td>
+              <td>1280x1080</td>
               <td>
                 <div class="radio">
                   <label>
@@ -90,7 +122,8 @@ const DownloadImage = ({ image }) => {
                       id="express"
                       name="optradio"
                       onClick={() => {
-                        setWidth(960);
+                        setLargeImageSelected(true);
+                        setWidth(1280);
                       }}
                     />
                   </label>
@@ -100,146 +133,30 @@ const DownloadImage = ({ image }) => {
           </tbody>
         </table>
       </form>
-      <Link
-        target="_blank"
-        href={`${downloadUrl}_${width}.jpg`}
-        download={`${downloadUrl}_${width}.jpg`}
+      <Button
+        mt={2}
+        size="lg"
+        w={"100%"}
+        colorScheme="green"
+        isLoading={loading}
+        loadingText={"loading..."}
+        onClick={() => {
+          handleImageDownload(downloadURL);
+        }}
       >
-        <Button mt={2} w={"100%"} colorScheme="green" size="lg">
-          Download for free!
-        </Button>
-      </Link>
+        Download for free!
+      </Button>
     </Box>
   );
 };
 
+function handleImageDownload(imageUrl) {
+  const linkEl = document.createElement("a");
+  linkEl.href = imageUrl;
+  linkEl.download = "image";
+  document.body.appendChild(linkEl);
+  linkEl.click();
+  document.body.removeChild(linkEl);
+}
+
 export default DownloadImage;
-
-// const RadioItem = ({ size, width, height }) => {
-//   return (
-//     <Box
-//       p={2}
-//       display={"flex"}
-//       alignItems={"flex-start"}
-//       justifyContent={"flex-start"}
-//     >
-//       <Text ml={1}>{size}</Text>
-//       <Text ml={2}>
-//         {width}x{height}
-//       </Text>
-//     </Box>
-//   );
-// };
-
-// const TableRow = ({ size, width, height }) => {
-//   return (
-//     <Tr>
-//       <Td>{size}</Td>
-//       <Td>
-//         <b>
-//           {width}*{height}
-//         </b>
-//       </Td>
-//     </Tr>
-//   );
-// };
-
-{
-  /* <TableContainer w={"100%"}>
-  <Table size={"sm"} variant="simple" colorScheme="teal">
-    <Thead>
-      <Tr>
-        <Th>Size</Th>
-        <Th>Dimensions</Th>
-        <Th></Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <form>
-        <Tr>
-          <Td>Small</Td>
-          <Td>ndkfnkdj</Td>
-          <Td>
-            <Radio value="1"></Radio>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>Medium</Td>
-          <Td>kldsdskfn</Td>
-          <Td>
-            <Radio value="2"></Radio>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>Big</Td>
-          <Td></Td>
-          <Td>
-            <Radio value="3"></Radio>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>Original</Td>
-          <Td></Td>
-          <Td>
-            <Radio value="4"></Radio>
-          </Td>
-        </Tr>
-      </form>
-    </Tbody>
-  </Table>
-</TableContainer>; */
-}
-
-{
-  /* <RadioGroup defaultValue="1" colorScheme="green">
-  <Box display="flex" flexDir="column">
-    <Radio value="1">
-      <RadioItem size={"Small"} width={640} height={960} />
-    </Radio>
-    <Radio value="2">
-      <RadioItem size={"Medium"} width={1920} height={2660} />
-    </Radio>
-    <Radio value="3">
-      <RadioItem size={"Big"} width={2400} height={3600} />
-    </Radio>
-    <Radio value="4">
-      <RadioItem
-        size={"Original"}
-        width={image.imageWidth}
-        height={image.imageHeight}
-      />
-    </Radio>
-  </Box>
-</RadioGroup>; */
-}
-
-{
-  /* <Tr>
-              <Td>One</Td>
-              <Td>two</Td>
-            </Tr>
-            <RadioGroup defaultValue="1" colorScheme="green">
-              <Radio value="1">
-                <TableRow size={"Small"} width={640} height={960} />
-              </Radio>
-              <Tr>
-                <Radio value="2">
-                  <TableRow size={"Medium"} width={1920} height={2660} />
-                </Radio>
-              </Tr>
-              <Tr>
-                <Radio value="3">
-                  <TableRow size={"Big"} width={2400} height={3600} />
-                </Radio>
-              </Tr>
-              <Tr>
-                <Radio value="4">
-                  <TableRow
-                    size={"Original"}
-                    width={image.imageWidth}
-                    height={image.imageHeight}
-                  />
-                </Radio>
-              </Tr>
-            </RadioGroup> */
-}
